08d635eb1e90aad07bc944a88523ca7bc0e45bab
thinkAmi <dev.thinkami+git@gmail.com>
2016-01-06 23:31:47 +0900

    p92-p94 チャットスライダーの拡大や格納を行うメソッドを追加

--- chap3/spa/js/spa.shell.js
+++ chap3/spa/js/spa.shell.js
@@ -28,7 +28,14 @@ spa.shell = (function(){
                 + '</div>'
                 + '<div class="spa-shell-foot"></div>'
                 + '<div class="spa-shell-chat"></div>'
-                + '<div class="spa-shell-modal"></div>'
+                + '<div class="spa-shell-modal"></div>',
+            
+            // 「開発者がスライダーの動きと高さを設定できるようにする」ために、
+            // モジュール構成マップで、拡大の時間と高さを設定する
+            chat_extend_time: 1000,
+            chat_retract_time: 300,
+            chat_extend_height: 450,
+            chat_retract_height: 15
         },
         // モジュール全体で共有する動的情報をstateMapに配置
         stateMap = { $container: null },
@@ -37,7 +44,10 @@ spa.shell = (function(){
         
         // このセクションですべてのモジュールスコープ変数を宣言する
         // その多くはのちに割り当てる
-        setJqueryMap, initModule;
+        setJqueryMap, 
+        toggleChat, // モジュールスコープ変数のリストにtoggleChatを追加 
+        initModule;
+        
         
         //----モジュールスコープ変数終了----
         
@@ -53,9 +63,66 @@ spa.shell = (function(){
         // 性能を改善できる
         setJqueryMap = function(){
             var $container = stateMap.$container;
-            jquerymap = { $container: $container };
+            jqueryMap = { 
+                $container: $container,
+                // チャットスライダーjQueryコレクションをjqueryMapにキャッシュ
+                $chat: $container.find('.spa-shell-chat')
+            };
         };
         //-------- DOMメソッド/setJqueryMap/終了
+        
+        // チャットスライダーの拡大や格納を行う1つのメソッドを作成するため、
+        // toggleChatメソッドを追加する
+        //-------- DOMメソッド/toggleChat/開始
+        // 目的：チャットスライダーの拡大や格納
+        // 引数：
+        //   * do_extend - trueの場合、スライダーを拡大する。falseの場合は格納する。
+        //   * callback - アニメーションの最後に実行するオプションの関数
+        // 設定：
+        //   * chat_extend_time, chat_retract_time,
+        //   * chat_extend_height, chat_retract_height
+        // 戻り値：boolean
+        //   * true - スライダーアニメーションが追加された
+        //   * false - スライダーアニメーションが開始されなかった
+        //
+        toggleChat = function(do_extend, callback){
+            var
+                px_chat_ht = jqueryMap.$chat.height(),
+                is_open = px_chat_ht === configMap.chat_extend_height,
+                is_closed = px_chat_ht === configMap.chat_retract_height,
+                is_sliding = ! is_open && ! is_closed;
+                
+            // 競合状態を避ける
+            // スライダーがすでに動作中の場合は、対処を拒否して競合状態を避ける
+            if (is_sliding) { return false; }
+            
+            //-------- チャットスライダーの拡大開始
+            if (do_extend){
+                jqueryMap.$chat.animate(
+                    { height: configMap.chat_extend_height },
+                    configMap.chat_extend_time,
+                    function(){
+                        // アニメーション終了時にコールバックを呼び出す
+                        if (callback){ callback(jqueryMap.$chat); }
+                    }
+                );
+                
+                return true;
+            }
+            //-------- チャットスライダーの拡大終了
+            
+            //-------- チャットスライダーの格納開始
+            jqueryMap.$chat.animate(
+                { height: configMap.chat_retract_height },
+                configMap.chat_retract_time,
+                function(){
+                    // こちらも、アニメーション終了時にコールバックを呼び出す
+                    if (callback){ callback(jqueryMap.$chat); }
+                }
+            );
+            //-------- チャットスライダーの格納開始
+        };
+        //-------- DOMメソッド/toggleChat/終了
         
         //---- イベントハンドラ開始 ----
         // jQueryイベントハンドラ関数のための「イベントハンドラ」セクションを用意する
@@ -66,9 +133,15 @@ spa.shell = (function(){
         // パブリックメソッド/initModule/開始
         // モジュールの初期化に使う
         initModule = function($container){
+            // HTMLをロードし、jQueryコレクションをマッピングする
             stateMap.$container = $container;
             $container.html(configMap.main_html);
             setJqueryMap();
+            
+            // 切り替えをテストする
+            // ページロードの3秒後にスライダーを拡大し、8秒後に格納する
+            setTimeout(function() { toggleChat(true); }, 3000);
+            setTimeout(function() { toggleChat(false); }, 8000);
         };
         // パブリックメソッド/initModule/終了
         // mapにパブリックメンバを戻すことで、明示的にパブリックメソッドをエクスポートする
